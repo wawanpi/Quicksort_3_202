dd76fbd5d7845981687b271065d789280ad99377
wawanpi <muhaguskurniawan@gmail.com>
2023-03-31 20:04:07 +0700

    void

--- QuickSort_D_202/QuickSort_D_202.cpp
+++ QuickSort_D_202/QuickSort_D_202.cpp
@@ -5,3 +5,21 @@ using namespace std;
 int arr[20];
 int cmp_count = 0; //number of comparaison
 int mov_count = 0; //number of comparaison
+int n;
+
+void input() {
+	while (true){
+		cout << "Masukan panjang element array :";
+		cin >> n;
+			if (n <= 20)
+				break;
+			else
+			
+				cout << "\n maksimum panjang arrary adalah 20" << endl;
+		}
+		cout << "\n--------------------------" << endl;
+		cout << "\nEnter array element" << endl;
+		cout << "\n--------------------------" << endl;
+
+	}
+}
